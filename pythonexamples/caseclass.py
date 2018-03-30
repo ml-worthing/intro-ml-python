8d1034c8d042ba30bc8def21f615882792b8f375
Pawel Panasewicz <pawel.panasewicz@gmail.com>
2018-03-30 18:26:13 +0100

    some more named tuple example

--- pythonexamples/caseclass.py
+++ pythonexamples/caseclass.py
@@ -12,3 +12,10 @@ print(p1 == p2) #True
 print(p2 == p3) #False
 print(p1 + p2)  #(11, 22, 11, 22)
 
+p5 = p1._replace(y=555) #copy
+print(p5) #Point(x=11, y=555)
+print(p1 == p5) #False
+
+
+#see https://docs.python.org/2/library/collections.html#collections.namedtuple
+
